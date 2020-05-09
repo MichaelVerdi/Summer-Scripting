f24de4c56ada07642d4e7b86c78abc8bdc2d8c26
mike <michael.verdi1028@gmail.com>
2020-05-09 17:09:06 -0400

    Updated for Mod security

--- Centos/Apache_Serv.sh
+++ Centos/Apache_Serv.sh
@@ -33,13 +33,37 @@ fi
 if(( `systemctl is-active httpd` == 0 ));
 then
    echo "Securing http"
+   
    cd /etc/httpd/conf
+   
    echo 'ServerTokens Prod' >> httpd.conf
+   
    echo 'ServerSignature Off' >> httpd.conf 
+   
    echo 'TraceEnable off' >> httpd.conf
+   
    sed -i '144s/Options Indexes FollowSymLinks/Options None/' httpd.conf
-  
+   
    systemctl restart httpd
+
 fi
+#Install Mod security
+if(( `systemctl is-active httpd` == 0 ));
+then
+  echo "Making and disabling base webpages"
+  
+  echo "This Page was made from your script">/var/www/html/index.html
+  
+  cd /etc/htpd/conf.d
+  
+  mv welcome.conf old_hpage
+  
+  yum install mod_security -y
+  
+  cd /etc/httpd/conf.d
+  
+  httpd -M | grep security
+  
+  systemctl restart httpd
 
-
+fi
